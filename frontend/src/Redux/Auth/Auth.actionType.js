78719f44cc98851407cde1d4213600d713bc4cd7
sandeepguptax2003 <sandeepguptax2003@gmail.com>
2023-07-19 09:51:34 +0530

    REDUX

---
+++ frontend/src/Redux/Auth/Auth.actionType.js
@@ -0,0 +1,12 @@
+//loading
+export const LOADING_TRUE = "loading/true";
+
+//Error
+export const ERROR_TRUE = "error/true";
+
+//login
+export const LOG_IN_SUCCESS = "auth/login/success";
+export const LOG_OUT_SUCCESS = "auth/logout/success";
+
+//sign In
+export const SIGN_UP_SUCCESS = "auth/signup/success";
